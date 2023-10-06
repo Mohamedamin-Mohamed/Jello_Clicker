5441a3b490096e285c19902dee8099625b17acc7
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2023-10-06 11:05:35 -0500

    Added a Mouse Event Listener

--- script1.js
+++ script1.js
@@ -1,80 +1,81 @@
+import { clickCounter, restartGame, domManipulation, continueGame} from './script.js';
+
 /*
  * Files: Main.js                  
  * Author: Mohamedamin Mohamed          
  * Contact mohamedamin204080@gmail.com  
  * Created 09/26/2023                   
  * Modified: 09/29/2023                    
- * Description: This JavaScript file contains a set of functions and event listeners to manage and enhance the functionality of a web-based clicker game. The game involves clicking an image, and based on the click count, various features and messages are displayed in the HTML document. It also provides a way to restart the game, manipulate the DOM, and control scrolling behavior. The file is organized with proper documentation-style comments to explain the purpose and usage of each function.         
-**/
+ * Description: File is an event handling script for the web-based clicker game. It handles the initialization of the game, tracks the number of clicks, and triggers various actions based on the click count. The script also manages alerts, DOM manipulation, and user interactions for restarting the game.             *
+ */
 
 /**
- * Updates the DOM and displays a message based on the click count.
- * @param {HTMLElement} div - The HTML element to be manipulated.
- * @param {number} counter - The current click count.
+ * Function to initialize and handle game events when the DOM is fully loaded.
  */
-export function domManipulation(div, counter) {
-    switch (counter) {
-      case 10:
-        alert("Congratulations! You've unlocked Feature 1.");
-        div.style.backgroundColor = "green";
-        break;
-      case 30:
-        alert("Great progress! You've unlocked Feature 2.");
-        div.style.backgroundColor = "green";
-        break;
-      case 60:
-        alert("Almost there! You've unlocked Feature 3.");
-        div.style.backgroundColor = "green";
-        break;
-      case 100:
-        alert("Congratulations! You've unlocked all features.");
-        div.style.backgroundColor = "green";
-        break;
-    }
-  }
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+let counter = 0;//will store the number of click counts
+let imageClick = document.getElementById("imageClicker");
+let text = document.getElementById("text");
+let container = document.getElementsByClassName("column1");
+let restartGameClick = document.getElementById("div5");
+let divPlay = document.getElementById("div7");
+let alertShown = false;
   
   /**
-   * Displays a message and continues the game in a designated HTML element.
-   * @param {HTMLElement} playDiv - The HTML element where the game continues.
+   * Event listener for click events on the image.
    */
-  export function continueGame(playDiv) {
-    let textLabel = document.createElement("p");
-    textLabel.textContent = "Playing for fun now, haah!";
-    textLabel.style.textAlign = "center";
-    textLabel.style.alignItems = "center";
-    textLabel.classList.add("bouncing-text");
-    playDiv.style.overflow = "hidden"; // Set overflow to hidden
-    playDiv.appendChild(textLabel);
+imageClick.addEventListener("click", ()=>{
+  // Update the click count and related elements.
+  counter = clickCounter(text, counter, divPlay);
+   // Check the click count and perform actions accordingly.
+  switch(counter){
+    case 10:
+      domManipulation(document.getElementById("div1"), counter);
+      break;
+    case 30:
+       domManipulation(document.getElementById("div2"), counter);
+      break;
+    case 60:
+       domManipulation(document.getElementById("div3"), counter);
+      break;
+    case 100:
+       domManipulation(document.getElementById("div4"), counter);
+      container[0].style.backgroundColor = "green";
+      
+      // Show a confirmation dialog after a delay.
+      setTimeout(() => {
+          const playAgain = confirm("You unlocked all points, do you want to play again!!!");
+          if (playAgain) {
+            restartGame();
+          }
+        else{
+          // Get the playDiv element and continue the game.
+          let playDiv = document.getElementById("div8");
+          continueGame(playDiv);
+        }
+        }, 2000);
+      break;
   }
+});
   
   /**
-   * Updates the click counter and displays the current click count.
-   * @param {HTMLElement} text - The HTML element where the click count is displayed.
-   * @param {number} counter - The current click count.
-   * @param {HTMLElement} divPlay - The HTML element where game continuation messages are displayed.
-   * @returns {number} The updated click count.
+   * Event listener for mouseover on the image.
    */
-  export function clickCounter(text, counter, divPlay) {
-    text.style.display = "block";
-    counter++;
-    text.style.textAlign = "center";
-    text.textContent = `${counter} click${counter === 1 ? '' : 's'}`;
+   imageClicker.addEventListener("mouseover", () => {
+     // Show an alert message only once.
+      if (!alertShown) {
+        setTimeout(() => {
+          alert("Click to unlock features. Reach 100 clicks to win the game!");
+          alertShown = true;
+        }, 1000);
+      }
+    });
   
-    if (counter === 100) {
-      let textLabel = document.createElement("p");
-      textLabel.textContent = "All features unlocked!!!";
-      textLabel.style.textAlign = "center";
-      textLabel.style.alignItems = "center";
-      divPlay.appendChild(textLabel);
-      divPlay.style.backgroundColor = "green";
-    }
-  
-    return counter;
-  }
-  
-  /**
-   * Restarts the game by reloading the page.
+   /**
+   * Event listener for the restart game button.
    */
-  export function restartGame() {
-    location.reload();
-  }
+  restartGameClick.addEventListener("click", ()=>{
+    restartGame();
+  });
+});
